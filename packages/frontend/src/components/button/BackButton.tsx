0e4fde385ef4f10c45d9ecd9b4dd8a377438b6fa
hankn01 <kangminpersonal@naver.com>
2023-09-03 04:24:27 +0900

    BackButton Text Modified

--- packages/frontend/src/components/button/BackButton.tsx
+++ packages/frontend/src/components/button/BackButton.tsx
@@ -4,7 +4,7 @@ function BackButton() {
     return (
         <div className="absolute rounded-full border border-gray-200 w-10 h-10 p-2">
             {/* <img src={BackArrowIcon}></img> */}
-            1212
+            {"<"}
         </div>
     )
 }
