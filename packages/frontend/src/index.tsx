3a7dc59c183e91502728ddc7782efe8b28002d8e
changeonPark <qkrcksrjs@gmail.com>
2023-09-03 05:48:36 +0900

    revert: login page

--- packages/frontend/src/index.tsx
+++ packages/frontend/src/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route, Outlet, useNavigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet, useNavigate, useLocation } from "react-router-dom";
 import Header from "./pages/Header";
 import Start from "./pages/Start";
 import Dashboard from "./pages/Dashboard";
@@ -30,36 +30,6 @@ export default function App() {
 }
 
 const AppLayout = observer(() => {
-  const navigate = useNavigate();
-  const userContext = useContext(User);
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    // 로딩 완료 후 실행될 로직
-    const checkSignedUp = async () => {
-      await userContext.load(); // 가정: hasSignedUp을 결정하는 비동기 함수
-      setIsLoading(false);
-    };
-
-    checkSignedUp();
-  }, [userContext]);
-
-  useEffect(() => {
-    if (isLoading) {
-      return; // 로딩 중일 때는 라우팅을 실행하지 않음
-    }
-
-    if (!userContext.hasSignedUp) {
-      navigate("/login");
-    } else {
-      navigate("/front");
-    }
-  }, [userContext.hasSignedUp, navigate, isLoading]);
-
-  if (isLoading) {
-    return <div className="w-screen h-screen bg-custom-gradient" />;
-  }
-
   return (
     <main className="mx-auto max-w-[313px] my-20">
       <Outlet />
