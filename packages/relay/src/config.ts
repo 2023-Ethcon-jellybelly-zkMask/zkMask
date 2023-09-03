dc77377a30f6b3305f1fa65f46d96985d22809fd
sangwonmoonkr <san4865@naver.com>
2023-09-03 11:42:02 +0900

    reset_contract

--- packages/relay/src/config.ts
+++ packages/relay/src/config.ts
@@ -3,8 +3,8 @@ import _config from '../../../config'
 import { config } from 'dotenv'
 config()
 
-export const UNIREP_ADDRESS = '0xC2cEAF60fA88aE56fD2eC3Fa2997034c1201bF3A'
-export const APP_ADDRESS = '0xf078c5Eb6925112c5155290eB53Fc1FDF5b1d47b'
+export const UNIREP_ADDRESS = '0x4D137bb44553d55AE6B28B5391c6f537b06C9cc3'
+export const APP_ADDRESS = '0x57D445fEA3c06b83C1a88ac00fbbdeA0b4cc90A6'
 export const ETH_PROVIDER_URL =
     process.env.ETH_PROVIDER_URL ?? _config.ETH_PROVIDER_URL
 export const PRIVATE_KEY = process.env.PRIVATE_KEY ?? _config.PRIVATE_KEY
