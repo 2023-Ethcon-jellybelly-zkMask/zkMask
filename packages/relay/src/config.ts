cffd483f421ec3d9906d273946a905021ed8fbfe
sangwonmoonkr <san4865@naver.com>
2023-09-03 11:10:47 +0900

    deploy_update

--- packages/relay/src/config.ts
+++ packages/relay/src/config.ts
@@ -4,7 +4,7 @@ import { config } from 'dotenv'
 config()
 
 export const UNIREP_ADDRESS = '0xC2cEAF60fA88aE56fD2eC3Fa2997034c1201bF3A'
-export const APP_ADDRESS = process.env.APP_ADDRESS ?? _config.APP_ADDRESS
+export const APP_ADDRESS = '0xf078c5Eb6925112c5155290eB53Fc1FDF5b1d47b'
 export const ETH_PROVIDER_URL =
     process.env.ETH_PROVIDER_URL ?? _config.ETH_PROVIDER_URL
 export const PRIVATE_KEY = process.env.PRIVATE_KEY ?? _config.PRIVATE_KEY
